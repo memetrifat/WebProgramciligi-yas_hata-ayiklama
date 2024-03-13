b72098ec6b13f2fb03077bcd429071d69dfd1ea2
memetrifat <146707615+memetrifat@users.noreply.github.com>
2024-03-13 14:03:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+ function yasHesapla($yil) {
+ if(is_string($yil)) {
+ throw new Exception("Sadece yıl bilgisi giriniz."); 
+ }
+ $sonuc=2024-$yil;
+ return $sonuc;
+ } 
+ try {
+ $yas=yasHesapla(1988);
+ echo "<b>Yaşınız=</b>".$yas."<br>";
+ $yas=yasHesapla(2001);
+ echo "<b>Yaşınız=</b>".$yas."<br>";
+ $yas=yasHesapla("ikibin");
+ echo "<b>Yaşınız=</b>".$yas; 
+ } 
+ catch(Exception $e) {
+ echo "<b style='color:red'>Hata mesajı:</b> " .$e->getMessage();
+ } 
+ finally
+ {
+ echo "<br>";
+ echo "<b style='background-color:orange'>
+ -Yaşamınızda mutluluklar dileriz.</b>";
+ }
+?>
